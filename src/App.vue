97950a398894630ec3558182affc200ff435e310
jacktbg <jack.bustinza.work@gmail.com>
2025-08-27 17:16:27 -0500

    feat: add global styles, update App component to render a container with max of 1366px, add a counter componenent and import into the app with styles

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import Counter from "./components/Counter.vue"
+</script>
+<template>
+  <div :class="$style.container">
+    <Counter />
+  </div>
+</template>
+
+<style module>
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+  import { ref } from "vue"
+
+  const count = ref<number>(0)
+
+  const increment = () => {
+    count.value++
+  }
+
+  const reset = () => {
+    count.value = 0
+  }
+</script>
+<template>
+  <div :class="$style.container">
+    <h1 :class="$style.title">Counter</h1>
+    <p :class="$style.counter">Count: {{ count }}</p>
+    <button
+      :class="$style.incrementButton"
+      @click="increment()"
+      >Count + 1</button
+    >
+    <button :class="$style.resetButton" @click="reset()"
+      >Reset</button
+    >
+  </div>
+</template>
+<style module>
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: var(--space-md);
+  }
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: var(--space-md);
+  }
+  .title {
+    font-size: var(--font-size-xxl);
+    font-weight: 600;
+  }
+  .p {
+    font-size: var(--font-size-md);
+    color: var(--color-text-muted);
+    font-weight: 300;
+  }
+  .incrementButton {
+    background: var(--color-primary);
+    font-size: var(--font-size-md);
+    font-weight: 700;
+    padding: var(--space-sm) var(--space-md);
+    border-radius: var(--space-xs);
+    color: var(--color-text-inverse);
+  }
+  .resetButton {
+    background: var(--color-warning);
+    font-size: var(--font-size-md);
+    font-weight: 700;
+    padding: var(--space-sm) var(--space-md);
+    border-radius: var(--space-xs);
+    color: var(--color-text-inverse);
+  }
+</style>
