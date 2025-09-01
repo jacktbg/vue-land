b414733f9dfa16118fcfd8ef02dc795192f14e02
jacktbg <jack.bustinza.work@gmail.com>
2025-09-01 18:14:54 -0500

    wip(todo): create todos header, add styles and layout styles for todos container

---
+++ src/components/three/Header.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+  const categories = ["---", "work", "home", "personal"]
+</script>
+
+<template>
+  <form :class="s.form">
+    <input type="text" :class="s.input" />
+    <div :class="s.wrapper">
+      <select name="" id="" :class="s.options">
+        <option
+          v-for="c in categories"
+          :key="c"
+          value="c"
+          :class="s.option"
+          >{{ c }}</option
+        >
+      </select>
+      <button :class="s.button" type="button">Add</button>
+    </div>
+  </form>
+</template>
+
+<style module="s">
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: var(--space-md);
+    width: 30%;
+  }
+
+  .input {
+    padding: var(--space-sm) var(--space-md);
+    border-radius: var(--space-xs);
+  }
+
+  .wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .options {
+    padding: var(--space-sm) var(--space-md);
+    border-radius: var(--space-xs);
+    background: var(--color-bg-alt);
+  }
+  .option {
+    padding: var(--space-sm) var(--space-md);
+    border-radius: var(--space-xs);
+  }
+  .button {
+    padding: var(--space-sm) var(--space-md);
+    border-radius: var(--space-xs);
+    background: var(--color-primary);
+    color: var(--color-text-inverse);
+    font-weight: bold;
+  }
+</style>
--- src/components/three/Todo.vue
+++ src/components/three/Todo.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Header from "./Header.vue"
+</script>
 
 <template>
-  <h1>Todo App</h1>
+  <div :class="s.container">
+    <h1 :class="s.title">Todo App</h1>
+    <Header />
+  </div>
 </template>
 
-<style module></style>
+<style module="s">
+  .container {
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: var(--space-lg);
+    width: 80%;
+    height: 95%;
+    padding: var(--space-md) 0;
+  }
+
+  .title {
+    color: var(--color-primary);
+    font-size: var(--font-size-3xl);
+  }
+</style>
